569e03f1e3551a5b694776250b390c3516055c8e
cristianISVRAND <cristianramirezsoto2003@gmail.com>
2025-06-24 03:34:59 -0400

    Subir carpeta docs con el contenido del portafolio

---
+++ docs/app/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'portafolio-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  // agrega aquí más recursos si usas imágenes o fuentes locales
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
